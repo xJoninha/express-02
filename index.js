a23e504ae4372d2007bd59faf763be792b43d5f6
xjoninha <jonasjfs15@gmail.com>
2022-04-06 21:55:33 -0300

    adicionando arquivo index.html como resposta do servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const path = require('path')
+
+const app = express()
+const port = 3000
+
+// app.get('/', (req, res) => res.send("Home!"))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'html/index.html')))
+
+app.get('/sobre', (req, res) => res.send("Sobre!"))
+app.listen(port, () => console.info(`Servidor rodando na porta ${port}`))
